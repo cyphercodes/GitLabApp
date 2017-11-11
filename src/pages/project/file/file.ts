fab9f119558b71a420026fa26944c19b4566f635
Rayan Salhab <rayansalhab@hotmail.com>
2017-11-11 19:18:39 +0200

    Added line numbers to files

--- src/pages/project/file/file.ts
+++ src/pages/project/file/file.ts
@@ -1,21 +1,20 @@
-import {Component, ElementRef, ViewChild} from "@angular/core";
+import {Component} from "@angular/core";
 import {NavParams} from "ionic-angular";
 import {Project} from "../../../common/services/Project";
 import {Api} from "../../../common/services/Api";
-import * as hljs from 'highlight.js';
+import * as hljs from "highlight.js";
+import {RepoTools} from "../../../common/tools/repo-tools/repo-tools";
 
 @Component({
   selector: 'page-file',
   templateUrl: 'file.html',
 })
 export class FilePage {
-  @ViewChild('code') codeElement: ElementRef;
   file = null;
   remote_file = null;
   content = '';
-  showCode = false;
 
-  constructor(public project: Project, private navParams: NavParams, private api: Api) {
+  constructor(public project: Project, private navParams: NavParams, private api: Api, private repoTools: RepoTools) {
   }
 
   ionViewDidLoad() {
@@ -24,11 +23,11 @@ export class FilePage {
     this.api.getFile(this.project.get().id, this.file.path, {params: {ref: 'master'}}).subscribe((data) => {
       console.log(data.json());
       this.remote_file = data.json();
-      this.content = atob(this.remote_file.content);
-      setTimeout(() => {
-        hljs.highlightBlock(this.codeElement.nativeElement);
-        this.showCode = true;
-      }, 1)
+      this.content = hljs.highlightAuto(this.repoTools.b64DecodeUnicode(this.remote_file.content)).value;
+      let tmpContent = '<span class="line">' + this.content;
+      tmpContent = tmpContent.replace(/(?:\r\n|\r|\n)/g, '</span> <span class="line">');
+      tmpContent = '</span>' + tmpContent;
+      this.content = tmpContent;
     }, (err) => {
 
     })
