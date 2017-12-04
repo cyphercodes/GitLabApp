0a8c6fff7f87b7a44d32a56b3fc71b5fb4e6429d
Rayan Salhab <rayansalhab@hotmail.com>
2017-12-04 22:10:11 +0200

    Improved commit page with highlightjs

--- src/pages/project/repository/commits/commit/commit.ts
+++ src/pages/project/repository/commits/commit/commit.ts
@@ -2,7 +2,8 @@ import {Component} from "@angular/core";
 import {NavParams} from "ionic-angular";
 import {Project} from "../../../../../common/services/Project";
 import {Api} from "../../../../../common/services/Api";
-import {Diff2Html} from 'diff2html'
+import {Diff2Html} from 'diff2html';
+import * as hljs from "highlight.js";
 
 @Component({
   selector: 'page-commit',
@@ -23,18 +24,27 @@ export class CommitPage {
       this.diffs = data.json();
       let diff = '';
       for (let onediff of this.diffs) {
+        console.log(onediff);
         diff = diff + `
 diff --git ` + onediff.old_path + ` ` + onediff.new_path + `
 ` + ((onediff.new_file) ? (`new file mode ` + onediff.b_mode) : (``)) + `
+` + ((onediff.deleted_file) ? (`deleted file mode ` + onediff.a_mode) : (``)) + `
 --- ` + onediff.old_path + `
 +++ ` + onediff.new_path + `
       ` + onediff.diff;
       }
+      console.log(diff);
       let outputHtml = Diff2Html.getPrettyHtml(diff, {
-        // showFiles: true,
+        showFiles: true,
+        // rawTemplates: {'generic-wrapper': "<div class=\"d2h-wrapme\">{{content}}</div>"}
       });
       this.outputHtmls.push(outputHtml);
-
+      setTimeout(() => {
+        var codeLines = document.getElementsByClassName("d2h-code-line-ctn");
+        [].forEach.call(codeLines, function (line) {
+          hljs.highlightBlock(line);
+        });
+      }, 5)
     });
   }
 
